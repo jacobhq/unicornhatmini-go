afe534a4396447cb95cecc09563061a20b3793da
Jacob Marshall <me@jhqcat.com>
2025-05-26 18:45:13 +0100

    It works!

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/physic"
 	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
 	"periph.io/x/host/v3"
 	"periph.io/x/host/v3/rpi"
-	"time"
 )
 
 const (
@@ -14,18 +22,24 @@ const (
 	CmdGlobalBrightness = 0x37
 	CmdComPinCtrl       = 0x41
 	CmdRowPinCtrl       = 0x42
-	CmdPinWriteDisplay  = 0x80
-	CmdPinReadDisplay   = 0x81
+	CmdWriteDisplay     = 0x80
+	CmdReadDisplay      = 0x81
 	CmdSystemCtrl       = 0x35
 	CmdScrollCtrl       = 0x20
 
 	Cols = 17
 	Rows = 7
 
-	SpiMaxSpeedHz = 600_000
+	SpiMaxSpeed = 600 * physic.KiloHertz
 )
 
-type Unicornhatmini struct {
+type Device struct {
+	Conn   spi.Conn
+	Pin    gpio.PinIO
+	Offset int
+}
+
+type UnicornHATMini struct {
 	rotation    int
 	disp        [Cols * Rows][3]uint8
 	buf         [28 * 8 * 2]uint8
@@ -34,19 +48,45 @@ type Unicornhatmini struct {
 	rightMatrix Device
 }
 
-type Device struct {
-	SpiDev spi.Conn
-	Pin    gpio.PinIO
-	Offset int
+func HSVToRGB(h, s, v float64) (uint8, uint8, uint8) {
+	h = math.Mod(h, 1.0)
+	if h < 0 {
+		h += 1.0
+	}
+
+	c := v * s
+	x := c * (1 - math.Abs(math.Mod(h*6, 2)-1))
+	m := v - c
+
+	var r, g, b float64
+	switch int(h * 6) {
+	case 0:
+		r, g, b = c, x, 0
+	case 1:
+		r, g, b = x, c, 0
+	case 2:
+		r, g, b = 0, c, x
+	case 3:
+		r, g, b = 0, x, c
+	case 4:
+		r, g, b = x, 0, c
+	case 5:
+		r, g, b = c, 0, x
+	}
+
+	return uint8((r + m) * 255), uint8((g + m) * 255), uint8((b + m) * 255)
 }
 
-func NewUnicornhatmini() *Unicornhatmini {
+func NewUnicornhatmini() (*UnicornHATMini, error) {
+	if _, err := host.Init(); err != nil {
+		return nil, fmt.Errorf("failed to initialize periph: %v", err)
+	}
 	var disp [Cols * Rows][3]uint8
 	for i := range disp {
 		disp[i] = [3]uint8{0, 0, 0}
 	}
 	var buf [28 * 8 * 2]uint8
-	lut := [119][3]int{
+	lut := [Cols * Rows][3]int{
 		{139, 138, 137}, {223, 222, 221}, {167, 166, 165}, {195, 194, 193},
 		{111, 110, 109}, {55, 54, 53}, {83, 82, 81}, {136, 135, 134},
 		{220, 219, 218}, {164, 163, 162}, {192, 191, 190}, {108, 107, 106},
@@ -78,10 +118,43 @@ func NewUnicornhatmini() *Unicornhatmini {
 		{352, 354, 353}, {436, 438, 437}, {380, 382, 381}, {408, 410, 409},
 		{324, 326, 325}, {268, 270, 269}, {296, 298, 297},
 	}
-	leftMatrix := Device{Pin: rpi.P1_8, Offset: 0}
-	rightMatrix := Device{Pin: rpi.P1_7, Offset: 28 * 8}
 
-	h := &Unicornhatmini{
+	leftPort, err := spireg.Open("SPI0.0")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open SPI0.0: %v", err)
+	}
+	leftConn, err := leftPort.Connect(SpiMaxSpeed, spi.Mode0, 8)
+
+	if err != nil {
+		return nil, fmt.Errorf("SPI0.0 Connect: %v", err)
+	}
+
+	rightPort, err := spireg.Open("SPI0.1")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open SPI0.1: %v", err)
+	}
+	rightConn, err := rightPort.Connect(SpiMaxSpeed, spi.Mode0, 8)
+
+	if err != nil {
+		return nil, fmt.Errorf("SPI0.1 Connect: %v", err)
+	}
+
+	leftPin := rpi.P1_24
+	rightPin := rpi.P1_26
+
+	leftMatrix := Device{
+		Conn:   leftConn,
+		Pin:    leftPin,
+		Offset: 0,
+	}
+
+	rightMatrix := Device{
+		Conn:   rightConn,
+		Pin:    rightPin,
+		Offset: 28 * 8,
+	}
+
+	h := &UnicornHATMini{
 		rotation:    0,
 		disp:        disp,
 		buf:         buf,
@@ -90,22 +163,29 @@ func NewUnicornhatmini() *Unicornhatmini {
 		rightMatrix: rightMatrix,
 	}
 
-	devices := []Device{leftMatrix, rightMatrix}
+	devices := []Device{h.leftMatrix, h.rightMatrix}
 	for _, d := range devices {
-		h.xfer(d, []byte{CmdSoftReset})
+		if err := h.xfer(d, []byte{CmdSoftReset}); err != nil {
+			return nil, fmt.Errorf("soft reset failed: %v", err)
+		}
+		time.Sleep(10 * time.Millisecond)
+
 		h.xfer(d, []byte{CmdGlobalBrightness, 0x01})
 		h.xfer(d, []byte{CmdScrollCtrl, 0x00})
 		h.xfer(d, []byte{CmdSystemCtrl, 0x00})
-		h.xfer(d, append([]byte{CmdPinWriteDisplay, 0x00}, h.buf[d.Offset:d.Offset+28*8]...))
+
+		writeCmd := append([]byte{CmdWriteDisplay, 0x00}, h.buf[d.Offset:d.Offset+28*8]...)
+		h.xfer(d, writeCmd)
+
 		h.xfer(d, []byte{CmdComPinCtrl, 0xff})
 		h.xfer(d, []byte{CmdRowPinCtrl, 0xff, 0xff, 0xff, 0xff})
 		h.xfer(d, []byte{CmdSystemCtrl, 0x03})
 	}
 
-	return h
+	return h, nil
 }
 
-func (h *Unicornhatmini) shutdown() {
+func (h *UnicornHATMini) Shutdown() {
 	devices := []Device{h.leftMatrix, h.rightMatrix}
 	for _, d := range devices {
 		h.xfer(d, []byte{CmdComPinCtrl, 0x00})
@@ -114,27 +194,28 @@ func (h *Unicornhatmini) shutdown() {
 	}
 }
 
-func (h *Unicornhatmini) exit() {
-	h.shutdown()
-}
-
-func (h *Unicornhatmini) xfer(d Device, data []byte) {
+func (h *UnicornHATMini) xfer(d Device, data []byte) error {
 	if err := d.Pin.Out(gpio.Low); err != nil {
-		fmt.Printf("Error pulling pin low: %v\n", err)
-		return
+		return fmt.Errorf("error pulling pin low: %v", err)
 	}
 
-	err := d.SpiDev.Tx(data, nil)
-	if err != nil {
-		fmt.Printf("SPI Tx error: %v\n", err)
+	if err := d.Conn.Tx(data, nil); err != nil {
+		d.Pin.Out(gpio.High)
+		return fmt.Errorf("SPI Tx error: %v", err)
 	}
 
 	if err := d.Pin.Out(gpio.High); err != nil {
-		fmt.Printf("Error releasing pin: %v\n", err)
+		return fmt.Errorf("error releasing pin: %v", err)
 	}
+
+	return nil
 }
 
-func (h *Unicornhatmini) setPixel(x, y int, r, g, b uint8) {
+func (h *UnicornHATMini) SetPixel(x, y int, r, g, b uint8) {
+	if x < 0 || x >= Cols || y < 0 || y >= Rows {
+		return
+	}
+
 	offset := x*Rows + y
 
 	switch h.rotation {
@@ -150,10 +231,12 @@ func (h *Unicornhatmini) setPixel(x, y int, r, g, b uint8) {
 		offset = y*Rows + x
 	}
 
-	h.disp[offset] = [3]uint8{r >> 2, g >> 2, b >> 2}
+	if offset >= 0 && offset < len(h.disp) {
+		h.disp[offset] = [3]uint8{r >> 2, g >> 2, b >> 2}
+	}
 }
 
-func (h *Unicornhatmini) setAll(r, g, b uint8) {
+func (h *UnicornHATMini) SetAll(r, g, b uint8) {
 	r >>= 2
 	g >>= 2
 	b >>= 2
@@ -163,11 +246,11 @@ func (h *Unicornhatmini) setAll(r, g, b uint8) {
 	}
 }
 
-func (h *Unicornhatmini) Clear() {
-	h.setAll(0, 0, 0)
+func (h *UnicornHATMini) Clear() {
+	h.SetAll(0, 0, 0)
 }
 
-func (h *Unicornhatmini) SetBrightness(b float64) {
+func (h *UnicornHATMini) SetBrightness(b float64) {
 	if b < 0 {
 		b = 0
 	} else if b > 1 {
@@ -181,7 +264,7 @@ func (h *Unicornhatmini) SetBrightness(b float64) {
 	}
 }
 
-func (h *Unicornhatmini) SetRotation(rotation int) error {
+func (h *UnicornHATMini) SetRotation(rotation int) error {
 	if rotation != 0 && rotation != 90 && rotation != 180 && rotation != 270 {
 		return fmt.Errorf("rotation must be one of 0, 90, 180, 270")
 	}
@@ -189,24 +272,37 @@ func (h *Unicornhatmini) SetRotation(rotation int) error {
 	return nil
 }
 
-func (h *Unicornhatmini) Show() {
+func (h *UnicornHATMini) Show() error {
 	for i := 0; i < Cols*Rows; i++ {
+		if i >= len(h.lut) {
+			continue
+		}
 		ir, ig, ib := h.lut[i][0], h.lut[i][1], h.lut[i][2]
 		r, g, b := h.disp[i][0], h.disp[i][1], h.disp[i][2]
-		h.buf[ir] = r
-		h.buf[ig] = g
-		h.buf[ib] = b
+
+		if ir < len(h.buf) && ig < len(h.buf) && ib < len(h.buf) {
+			h.buf[ir] = r
+			h.buf[ig] = g
+			h.buf[ib] = b
+		}
 	}
 
 	devices := []Device{h.leftMatrix, h.rightMatrix}
 	for _, d := range devices {
 		start := d.Offset
 		end := start + (28 * 8)
-		h.xfer(d, append([]byte{CmdPinWriteDisplay, 0x00}, h.buf[start:end]...))
+		if end > len(h.buf) {
+			end = len(h.buf)
+		}
+		writeCmd := append([]byte{CmdWriteDisplay, 0x00}, h.buf[start:end]...)
+		if err := h.xfer(d, writeCmd); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func (h *Unicornhatmini) GetShape() (int, int) {
+func (h *UnicornHATMini) GetShape() (int, int) {
 	if h.rotation == 90 || h.rotation == 270 {
 		return Rows, Cols
 	}
@@ -214,10 +310,45 @@ func (h *Unicornhatmini) GetShape() (int, int) {
 }
 
 func main() {
-	host.Init()
-	t := time.NewTicker(500 * time.Millisecond)
-	for l := gpio.Low; ; l = !l {
-		rpi.P1_33.Out(l)
-		<-t.C
+	unicorn, err := NewUnicornhatmini()
+	if err != nil {
+		log.Fatalf("Failed to initialize UnicornHATMini: %v", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\nShutting down...")
+		unicorn.Clear()
+		unicorn.Show()
+		unicorn.Shutdown()
+		os.Exit(0)
+	}()
+
+	unicorn.SetBrightness(0.2)
+
+	ticker := time.NewTicker(time.Second / 60)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			currentTime := float64(time.Now().UnixNano()) / 1e9
+
+			for y := 0; y < Rows; y++ {
+				for x := 0; x < Cols; x++ {
+					hue := (currentTime / 4.0) + (float64(x) / float64(Cols*2)) + (float64(y) / float64(Rows))
+					r, g, b := HSVToRGB(hue, 1.0, 1.0)
+					unicorn.SetPixel(x, y, r, g, b)
+				}
+			}
+
+			if err := unicorn.Show(); err != nil {
+				fmt.Printf("Error showing display: %v\n", err)
+			}
+		case <-c:
+			return
+		}
 	}
 }
